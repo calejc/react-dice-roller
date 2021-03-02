b4a3679087120a296c953a54b302b550d59e1ca7
Cale Cortney <thortney@mailbox.org>
2021-03-02 05:51:15 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import {diceArt} from './data';
 
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Dice />
     </div>
   );
 }
 
+function Dice(){
+  const [diceValue, setDice] = useState({dice1:0, dice2:0});
+
+  function Roll(){
+    const min = 1;
+    const max = 6;
+    const rand1 = Math.round(min + Math.random() * (max-min));
+    const rand2 = Math.round(min + Math.random() * (max-min));
+    setDice({dice1: rand1, dice2: rand2})
+  }
+
+  return (
+    <div id="container">
+      <div id="dice-1">
+        {diceArt[diceValue.dice1]}
+      </div>
+      <div id="button">
+        <button onClick={Roll}>ROLL</button>
+      </div>
+      <div id="dice-2">
+        {diceArt[diceValue.dice2]}
+      </div>
+    </div>
+  )
+}
+
 export default App;
+
